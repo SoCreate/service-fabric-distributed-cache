6bbf04dcb317fd228a64f9e21b8aeeee7e8974c9
Dana.Desrosiers <Dana.Desrosiers@SoCreate.it>
2019-12-16 12:14:45 -0800

    upgrade Service Fabric to v4

--- tests/SoCreate.Extensions.Caching.Tests/AutoMoqDataAttribute.cs
+++ tests/SoCreate.Extensions.Caching.Tests/AutoMoqDataAttribute.cs
@@ -1,12 +1,18 @@
 ﻿using AutoFixture;
 using AutoFixture.AutoMoq;
 using AutoFixture.Xunit2;
+using ServiceFabric.Mocks;
 
 namespace SoCreate.Extensions.Caching.Tests
 {
     public class AutoMoqDataAttribute : AutoDataAttribute
     {
-        public AutoMoqDataAttribute() : base(() => new Fixture().Customize(new AutoMoqCustomization { GenerateDelegates = true }))
+        public AutoMoqDataAttribute() : base(() =>
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization {GenerateDelegates = true});
+            fixture.Register(() => MockStatefulServiceContextFactory.Default);
+            return fixture;
+        })
         {
         }
     }
